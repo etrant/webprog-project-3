8f9d1e2a6bf1df94f9d6b46a95bcfd7c43b9ff94
Ethan <30396562+etrant@users.noreply.github.com>
2024-04-26 16:23:39 -0400

    Homepage navbar

---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="./css/global.css">
+    <link rel="stylesheet" href="./css/index.css">
+</head>
+
+<body>
+    <header>
+        <nav class="navbar">
+            <ul class="navlinks">
+                <li><a href="">Log in</a></li>
+                <li><a href="">Sign up</a></li>
+            </ul>
+        </nav>
+    </header>
+    <div class="outside">
+        <div class="inside">
+        </div>
+    </div>
+</body>
+
+</html>
